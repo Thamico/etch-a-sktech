4a50d500fa0c72deebb9bcd15f2d849333216701
Thamico <thanh.nguyenctn1999@gmail.com>
2022-10-26 18:30:12 +0200

    drawing possible now

--- script.js
+++ script.js
@@ -5,13 +5,14 @@ function makeRows(rows, cols) {
   container.style.setProperty("--grid-cols", cols);
   for (c = 0; c < rows * cols; c++) {
     let cell = document.createElement("div");
-    cell.innerText = c + 1;
+    cell.addEventListener("mouseover", function colorGrid() {
+      cell.style.backgroundColor = "yellow";
+    });
     container.appendChild(cell).className = "grid-item";
   }
 }
 
 function getGrid() {
-  let x;
   let grid = prompt("Please enter how many grids one site has");
   if (grid != null) {
     makeRows(grid, grid);
